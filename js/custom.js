ff662861cdc52f1fd6a9169526abc36274ab8245
Edward-Shawn <sbh3834@naver.com>
2020-09-10 00:18:13 +0900

    오리진 반응형 제작 준비

---
+++ js/custom.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+    
+    
+
+    $(".owl-carousel").mouseenter(function(){
+        
+        $(".owl-nav button").css('opacity','1');
+    })
+    $(".owl-carousel").mouseleave(function(){
+        $(".owl-nav button").css('opacity','0');
+    })
+
+
+    $(".owl-carousel").owlCarousel({
+        items:1,
+        loop:true,
+        nav:true,
+        dots:true,
+        autoplay:true,
+        autoplaySpeed:1000,
+        smartSpeed:1500,
+        autoplayHoverPause:true
+    });
+   
+ 
+
+
+
+
+
+
+});
