0cbca5fed827158aa4ca8bf3846da210f872b2df
Edward-Shawn <sbh3834@naver.com>
2020-09-16 22:06:31 +0900

    모바일 화면 수정

--- js/custom.js
+++ js/custom.js
@@ -1,34 +1,39 @@
 $(document).ready(function(){
+
+    $(".menu-btn").click(function(){
+        if($(".menu-btn").hasClass("on") == false){
+            $(this).addClass("on");
+            $(".mobile").addClass("on");
+            $(".logo").css('left','200%');
+            $(".menu .search").css('right','-1000px');
+        }else{
+            $(this).removeClass("on");
+            $(".mobile").removeClass("on");
+            $(".logo").css('left','50%');
+            $(".menu .search").css('right','30px');
+        }
+
+    });
     
+
+    
+
+    
+
     $(".menu .search i").click(function(){
         $(".search").addClass("active");
+        $(".menu .logo").css('margin-top','-60px');
     })
-
-    $(".owl-carousel").mouseenter(function(){
-        
-        $(".owl-nav button").css('opacity','1');
-    })
-    $(".owl-carousel").mouseleave(function(){
-        $(".owl-nav button").css('opacity','0');
-    })
-
-
-    $(".owl-carousel").owlCarousel({
-        items:1,
-        loop:true,
-        nav:true,
-        dots:true,
-        autoplay: false,
-        autoplaySpeed:1000,
-        smartSpeed:1500,
-        autoplayHoverPause:true
-    });
    
- 
-
-
-
-
+    
+    $(document).click(function(e){
+        if($(e.target).closest(".search").length != 0){
+            return false;
+        }else if(!$(".search").val()){
+            $(".search").removeClass("active");
+            $(".menu .logo").css('margin-top','0');
+        }
+    });
 
 
 });
